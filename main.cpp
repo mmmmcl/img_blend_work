7638a3d0353e0761ab3c8e1f8b65dbb8022e6896
gyt <vocvocal@163.com>
2024-04-21 21:56:46 +0800

    计算bel pl

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Date: 2024-04-20 22:20:04
  * @LastEditors: gyt vocvocal@163.com
- * @LastEditTime: 2024-04-21 19:32:27
+ * @LastEditTime: 2024-04-21 21:55:01
  * @FilePath: /fusion_dempster/main.cpp
  */
 #include <iostream>
@@ -16,13 +16,12 @@ std::vector<double> combine_dempster_two(const std::vector<double> &bba1, const 
     std::vector<double> combined(NUM_SUBSETS, 0.0);
     double K = 0.0;
 
-
     for (int i = 1; i < NUM_SUBSETS; i++)
     {
         for (int j = 1; j < NUM_SUBSETS; j++)
         {
-            if ((i & j) == 0)  // i和j完全冲突
-            { 
+            if ((i & j) == 0) // i和j完全冲突
+            {
                 K += bba1[i] * bba2[j];
             }
         }
@@ -32,10 +31,10 @@ std::vector<double> combine_dempster_two(const std::vector<double> &bba1, const 
     {
         for (int j = 1; j < NUM_SUBSETS; j++)
         {
-            if ((i & j) != 0)  // i和j有交集
-            { 
+            if ((i & j) != 0) // i和j有交集
+            {
                 int indx = i & j;
-                combined[indx] += (bba1[i] * bba2[j]) / (1.0 - K); 
+                combined[indx] += (bba1[i] * bba2[j]) / (1.0 - K);
             }
         }
     }
@@ -48,11 +47,10 @@ std::vector<double> combine_dempster_multi(const std::vector<std::vector<double>
 {
     if (bba_sources.empty())
     {
-        std::cout<<"证据源空"<<std::endl;
+        std::cout << "证据源空" << std::endl;
         std::exit(-1);
     }
     std::vector<double> combined = bba_sources[0];
-
 
     // 递归结合剩余的证据源
     for (size_t i = 1; i < bba_sources.size(); ++i)
@@ -60,6 +58,28 @@ std::vector<double> combine_dempster_multi(const std::vector<std::vector<double>
         combined = combine_dempster_two(combined, bba_sources[i]);
     }
     return combined;
+}
+void calculate_bel_and_pl(const std::vector<double> &combined, std::vector<double> &bel, std::vector<double> &pl)
+{
+    for (int i = 0; i < NUM_SUBSETS; i++)
+    {
+
+        for (int j = 0; j < NUM_SUBSETS; j++)
+        {
+            if ((i & j) == j)
+            {
+                bel[i] += combined[j];
+            }
+        }
+
+        for (int j = 0; j < NUM_SUBSETS; j++)
+        {
+            if ((i & j) != 0)
+            {
+                pl[i] += combined[j];
+            }
+        }
+    }
 }
 
 int main()
@@ -72,9 +92,23 @@ int main()
 
     std::vector<double> combined = combine_dempster_multi(bba_sources);
 
-    std::cout << "Combined BBA from multiple sources: "<<std::endl;
+    std::cout << "Combined BBA from multiple sources: " << std::endl;
     std::cout << "空集：" << combined[0] << " 本拉登：" << combined[1] << " 萨达姆：" << combined[2] << " 本、萨：" << combined[3]
               << " 霍梅尼：" << combined[4] << " 本、霍：" << combined[5] << " 霍、萨：" << combined[6] << " 全集：" << combined[7] << std::endl;
-
+    std::vector<double> bel(NUM_SUBSETS, 0.0);
+    std::vector<double> pl(NUM_SUBSETS, 0.0);
+    calculate_bel_and_pl(combined, bel, pl);
+    std::cout << "Combined Beliefs:" << std::endl;
+    for (int i = 0; i < NUM_SUBSETS; i++)
+    {
+        std::cout <<bel[i]<<" ";
+    }
+    std::cout << std::endl;
+    std::cout << "Combined Plausibilities:" << std::endl;
+    for (int i = 0; i < NUM_SUBSETS; i++)
+    {
+          std::cout<<pl[i]<<" ";
+    }
+     std::cout << std::endl;
     return 0;
 }
